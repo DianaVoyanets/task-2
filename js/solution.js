da6a99a32800ea2e4440e38683540d756e7e4305
d.voyanets <diana.voyanets@gmail.com>
2018-02-15 22:53:53 +0300

    Task solution done

--- js/solution.js
+++ js/solution.js
@@ -10,8 +10,35 @@
      * @returns {number} кол-во островов
      */
     function solution(map) {
-        // todo: подсчитать кол-во островов на карте
-        return 0;
+        var count = 0;
+
+        var boundary = (row, col) => [
+            [row - 1, col],
+            [row, col - 1],
+            [row + 1, col],
+            [row, col + 1],
+        ];
+
+        var visited = map.map(row => row.map(() => false));
+
+        function dfs(row, col) {
+            if (visited[row][col]) return false;
+
+            visited[row][col] = true;
+
+            if (map[row][col] == WATER) return false;
+
+            boundary(row, col)
+                .filter(cell => cell[0] >= 0 && cell[0] < map.length && cell[1] >= 0 && cell[1] < map[0].length)
+                .filter(cell => map[cell[0]][cell[1]] == ISLAND)
+                .forEach(cell => dfs(...cell));
+
+            return true;
+        }
+        
+        map.forEach((row, i) => row.forEach((cell, j) => dfs(i, j) && count++));
+
+        return count;
     }
 
     root.SHRI_ISLANDS.solution = solution;
